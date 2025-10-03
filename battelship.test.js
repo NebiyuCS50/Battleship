0a67b1a82b0f43fed2b2906939883839e8897f14
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-10-03 11:07:36 +0300

    feat: add test cases to test ship class

--- battelship.test.js
+++ battelship.test.js
@@ -1,1 +1,20 @@
+const { ship } = require("./battelship");
 
+test("Ship hit method increases hit count", () => {
+  const myShip = new ship(3);
+  myShip.hit();
+  expect(myShip.hitCount).toBe(1);
+});
+
+test("Ship isSunk method returns false when not sunk", () => {
+  const myShip = new ship(3);
+  myShip.hit();
+  expect(myShip.isSunk()).toBe(false);
+});
+
+test("Ship isSunk method returns true when sunk", () => {
+  const myShip = new ship(2);
+  myShip.hit();
+  myShip.hit();
+  expect(myShip.isSunk()).toBe(true);
+});
